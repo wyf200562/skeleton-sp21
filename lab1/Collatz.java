e4fd0480b68d18584cbf260e2d302fd035ffb6c7
wyf <wyf200562@163.com>
2025-10-13 13:24:45 +0800

    lab1
    Collatz sequence

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,16 +3,20 @@
  */
 public class Collatz {
 
+
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
+        if (n % 2 == 0){
+            n = n / 2;
+            return n;
+        }else if (n % 2 != 0){
+            n = 3 * n + 1;
+            return n;
+        }else {
             return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
         }
     }
+
 
     public static void main(String[] args) {
         int n = 5;
